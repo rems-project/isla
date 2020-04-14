839308712d27158d9c7eab2c466af4b54a8f84bc
Thibaut Pérami <thibaut.perami@ens.fr>
2020-04-14 16:03:21 +0200

    isla-lang: update build system to dune

--- isla-lang/Makefile
+++ isla-lang/Makefile
@@ -1,59 +1,36 @@
-#  make -C ../.. && make clean && make
-
-ROOT            := isla_lang
-
-OTTDIR          := ../../ott
-
-OTT             := $(OTTDIR)/bin/ott
-
-OTTFLAGS	::= -show_sort false -quotient_rules false -aux_style_rules false
-
-MENHIR          := menhir
-
-MENHIRFLAGS     := --infer --base $(ROOT)_parser
-# add --trace for menhir parse tracing
-#MENHIRFLAGS     := --trace --infer --base $(ROOT)_parser
-
-MENHIR_EXTRA_LIB:= $(OTTDIR)/menhir/menhir_library_extra.mly
-
-OCAMLBUILD      := ocamlbuild -use-ocamlfind -use-menhir -menhir "$(MENHIR) $(MENHIRFLAGS) ../$(MENHIR_EXTRA_LIB) "
-
-MAIN            := main
-
-all: $(ROOT)_ast.ml $(ROOT)_parser.mly $(ROOT)_parser_pp.ml $(ROOT)_lexer.mll main.ml
-	$(OCAMLBUILD) $(MAIN).byte
-
-lib: $(ROOT)_ast.ml $(ROOT)_parser.mly $(ROOT)_parser_pp.ml $(ROOT)_lexer.mll
-	$(OCAMLBUILD) $(ROOT).cma $(ROOT).cmxa
+.PHONY: all main lib opam pdf clean realclean install uninstall
 
 
+all: main lib opam
 
-pdf: $(ROOT)_quotiented.pdf $(ROOT)_unquotiented.pdf
+main:
+	@dune build main.exe
 
-# generate the ocaml AST type, ocamllex lexer, menhir parser, and ocaml pretty printers for the AST, all from the Ott soruce
-$(ROOT)_ast.ml  $(ROOT)_lexer.mll $(ROOT)_parser.mly $(ROOT)_parser_pp.ml $(ROOT)_ast.tex : $(ROOT).ott
-	$(OTT) $(OTTFLAGS) -i $(ROOT).ott  -o $(ROOT)_parser.mly -o $(ROOT)_lexer.mll -o $(ROOT)_ast.ml -o $(ROOT).tex
+lib:
+	@dune build isla_lang.a
 
-$(ROOT)_quotiented.pdf: $(ROOT).ott Makefile
-	$(OTT) -quotient_rules true -i $(ROOT).ott -o $(ROOT)_quotiented.tex
-	pdflatex $(ROOT)_quotiented.tex
+opam:
+	@dune build @install
 
-$(ROOT)_unquotiented.pdf: $(ROOT).ott Makefile
-	$(OTT) -quotient_rules false -i $(ROOT).ott -o $(ROOT)_unquotiented.tex
-	pdflatex $(ROOT)_unquotiented.tex
+pdf: isla_lang_quotiented.pdf isla_lang_unquotiented.pdf
+
+isla_lang_quotiented.pdf: isla_lang.ott
+	@dune build $@
+
+isla_lang_unquotiented.pdf: isla_lang.ott
+	@dune build $@
 
 clean:
-	rm -rf *~
-	rm -rf _build
-	rm -rf $(ROOT)_ast.ml $(ROOT)_parser.mly $(ROOT)_lexer.mll $(ROOT)_parser_pp.ml
-	rm -rf *.aux *.log *.tex
-	rm -rf main.native main.byte
-	$(OCAMLBUILD) -clean
+	@dune clean
 
-realclean:
-	$(MAKE) clean
-	rm -rf *.pdf
+realclean: clean
+	@rm -rf *.pdf
 
-install: lib
-	ocamlfind remove isla-lang
-	ocamlfind install isla-lang META _build/$(ROOT).* _build/*.mli _build/*.cmi _build/*.cmx
+install: opam
+	@dune install
+
+uninstall: opam
+	@dune uninstall
+
+format:
+	@dune build @fmt
--- isla-lang/main.ml
+++ isla-lang/main.ml
@@ -1,33 +1,35 @@
 open Isla_lang
 
+let input_file = ref (None : string option)
 
-let input_file = ref( None : string option)
+let opts =
+  [("-i", Arg.String (fun s -> input_file := Some s), Printf.sprintf "<string> input file")]
 
-let opts = [
-  ("-i",
-    Arg.String (fun s -> input_file := Some s),
-    Printf.sprintf "<string> input file");
-  ]
-
-let usage = "Usage: just main (for interactive) or main -i <filename> (for batch)\n" ^
-              "       main -help   to show options"
+let usage =
+  "Usage: just main (for interactive) or main -i <filename> (for batch)\n"
+  ^ "       main -help   to show options"
 
 let usage' = usage ^ "\n" (*^ "Options:"*)
-let help outchan msg =
-  Printf.fprintf outchan "%s\n\n" msg
-let collect_file s = help stderr "illegal argument"; exit 1
+
+let help outchan msg = Printf.fprintf outchan "%s\n\n" msg
+
+let collect_file s =
+  help stderr "illegal argument";
+  exit 1
 
 let _ =
-  begin try Arg.parse_argv Sys.argv (Arg.align opts) collect_file usage' with
-        | Arg.Bad msg  -> help stderr msg; exit 1
-        | Arg.Help msg -> help stdout msg; exit 0
-  end
-
+  try Arg.parse_argv Sys.argv (Arg.align opts) collect_file usage' with
+  | Arg.Bad msg ->
+      help stderr msg;
+      exit 1
+  | Arg.Help msg ->
+      help stdout msg;
+      exit 0
 
 let process linebuf =
+  (* Run the generated lexer and parser on this input *)
   try
-    (* Run the generated lexer and parser on this input *)
-    let t = (Parser.trc_start Lexer.token linebuf) in
+    let t = Parser.trc_start Lexer.token linebuf in
     (* Show the generated raw and "pretty" pp of the result *)
     Printf.printf "   ";
     PPrintEngine.ToChannel.compact stdout (PP.pp_raw_trc PPrint.string t);
@@ -36,24 +38,24 @@ let process linebuf =
     PPrintEngine.ToChannel.compact stdout (PP.pp_trc PPrint.string t);
     Printf.printf "\n"
   with
-  | Lexer.Error msg ->
-      Printf.fprintf stdout "%s" msg
+  | Lexer.Error msg -> Printf.fprintf stdout "%s" msg
   | Parser.Error ->
-      Printf.fprintf stdout "%s^\nAt offset %d: syntax error.\n" (String.make (Lexing.lexeme_start linebuf) ' ') (Lexing.lexeme_start linebuf)
-
+      Printf.fprintf stdout "%s^\nAt offset %d: syntax error.\n"
+        (String.make (Lexing.lexeme_start linebuf) ' ')
+        (Lexing.lexeme_start linebuf)
 
 let _ =
   match !input_file with
   | None ->
-     begin
-       Printf.printf "enter isla traces\n";
-       let rec foo () =
-         let line = read_line () in
-         let linebuf = Lexing.from_string line in
-         process linebuf; flush stdout in
-       foo ()
-     end
+      Printf.printf "enter isla traces\n";
+      let foo () =
+        let line = read_line () in
+        let linebuf = Lexing.from_string line in
+        process linebuf;
+        flush stdout
+      in
+      foo ()
   | Some f ->
-     let c = open_in f in
-     let linebuf = Lexing.from_channel c in
-     process linebuf
+      let c = open_in f in
+      let linebuf = Lexing.from_channel c in
+      process linebuf
