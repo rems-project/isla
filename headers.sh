e29b93ac7d45444fa33936030c342bee812cc18e
Alasdair <alasdair.armstrong@cl.cam.ac.uk>
2020-05-29 15:49:20 +0100

    Update license to BSD 2-Clause
    Commits with the following string are ignored when updating the author
    list in the header, as running this script will touch all the files.
    
    LICENSE_UPDATE

---
+++ headers.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+bsd_license() {
+    cat <<EOF
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+EOF
+}
+
+strip_header() {
+    awk '(NR == 1 && /^[/][/]/), /^$/' "$1" | wc -l
+}
+
+author_info() {
+    printf "$1 BSD 2-Clause License\n$1\n"
+    local authors=$(git log --invert-grep --grep LICENSE_UPDATE --follow --pretty=format:'%aN' "$2" \
+                        | sort | uniq -c | sort -t ' ' -k 1 -n -r | sed 's/^\s*[0-9]\+\s*//')
+    while IFS= read -r author; do
+        local years=$(git log --follow --author="$author" --pretty=format:'%ad' --date=format:'%Y' "$2" \
+                          | sort -n | uniq | perl -pe 'chomp if eof' | tr '\n' ',' | sed 's/,/, /g')
+        printf "$1 Copyright (c) $years $author\n"
+    done <<< "$authors"
+    printf "$1\n"
+    bsd_license | awk "{print \"$1 \" \$0}"
+}
+
+author_info_global() {
+    printf "BSD 2-Clause License\n\n"
+    local authors=$(git log --invert-grep --grep LICENSE_UPDATE --pretty=format:'%aN' \
+                        | sort | uniq -c | sort -t ' ' -k 1 -n -r | sed 's/^\s*[0-9]\+\s*//')
+    while IFS= read -r author; do
+        local years=$(git log --author="$author" --pretty=format:'%ad' --date=format:'%Y' \
+                          | sort -n | uniq | perl -pe 'chomp if eof' | tr '\n' ',' | sed 's/,/, /g')
+        printf "Copyright (c) $years $author\n"
+    done <<< "$authors"
+    printf "\n"
+    bsd_license
+}
+
+main() {
+    while IFS= read -r file; do
+        local old_header_len=$(strip_header "$file")
+        local new_body=$(tail -n +"$old_header_len" "$file")
+        local new_header=$(author_info '//' "$file")
+        echo -n "$new_header" > "$file.nh"
+        printf "\n" >> "$file.nh"
+        echo -n "$new_body" >> "$file.nh"
+        printf '\n' >> "$file.nh"
+        mv "$file.nh" "$file"
+    done <<< $(git ls-files '*.rs' '*.lalrpop')
+
+    author_info_global '' '' > LICENSE
+}
+
+main
+
